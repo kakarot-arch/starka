0098569e59c88d7f9891a1eeb38f8e085cfe469e
kakarot-arch <kakarot8363@gmail.com>
2025-01-14 19:28:04 +0530

    packages update

---
+++ packages/client-farcaster/dist/ccip-WVBY25AJ.js
@@ -0,0 +1,14 @@
+import {
+  ccipRequest,
+  offchainLookup,
+  offchainLookupAbiItem,
+  offchainLookupSignature
+} from "./chunk-RQVQE6FS.js";
+import "./chunk-PR4QN5HX.js";
+export {
+  ccipRequest,
+  offchainLookup,
+  offchainLookupAbiItem,
+  offchainLookupSignature
+};
+//# sourceMappingURL=ccip-WVBY25AJ.js.map
